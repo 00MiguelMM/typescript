479a280a75a2c89d7499da2ddb7596c9ca56ca28
00MiguelMM <miguelmn637@gmail.com>
2026-04-15 12:14:25 +0200

    implementar funcion generarReporte

---
+++ modulo-2/src/reporte.ts
@@ -0,0 +1,14 @@
+import type { EstadoMatricula } from "./domain/types/estado-matricula";
+
+export function generarReporte(estado: EstadoMatricula): string {
+  switch (estado.tipo) {
+    case "ACTIVA":
+      return `Matrícula activa con ${estado.asignaturas.length} asignaturas`;
+
+    case "SUSPENDIDA":
+      return `Matrícula suspendida: ${estado.motivo}`;
+
+    case "FINALIZADA":
+      return `Matrícula finalizada con nota media ${estado.notaMedia}`;
+  }
+}
